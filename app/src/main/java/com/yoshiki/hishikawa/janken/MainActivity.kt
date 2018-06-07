04119030b7e923528cd660b2e50b2288892f2d73
‘yhishi’ <hishikawakeiki@hishi-Mac.local>
2018-06-07 21:58:37 +0900

    勝利確率ロジック追加

--- app/src/main/java/com/yoshiki/hishikawa/janken/MainActivity.kt
+++ app/src/main/java/com/yoshiki/hishikawa/janken/MainActivity.kt
@@ -3,6 +3,7 @@ package com.yoshiki.hishikawa.janken
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.View
 
 // Kotlin Android拡張プラグイン（ビューの取得簡略化）
@@ -17,6 +18,11 @@ class MainActivity : AppCompatActivity() {
         gu.setOnClickListener { onJankenButtonTapped(it) }  // itはタップされたViewオブジェクト
         choki.setOnClickListener { onJankenButtonTapped(it) }
         pa.setOnClickListener { onJankenButtonTapped(it) }
+
+        // アプリ起動時に共有プリファレンスを初期化
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+        editor.clear().apply()
     }
 
     fun onJankenButtonTapped(view: View?) {
--- app/src/main/java/com/yoshiki/hishikawa/janken/ResultActivity.kt
+++ app/src/main/java/com/yoshiki/hishikawa/janken/ResultActivity.kt
@@ -2,6 +2,7 @@ package com.yoshiki.hishikawa.janken
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
@@ -36,13 +37,16 @@ class ResultActivity : AppCompatActivity() {
             else -> gu
         }
 
+        // コンピュータの手を過去の結果から決定
+        val comHand = getComHand()
+
         // コンピュータの手をランダム表示
-        val comHand = (Math.random() * 3).toInt()
-        when(comHand) {
-            gu -> comHandImage.setImageResource(R.drawable.com_gu)
-            choki -> comHandImage.setImageResource(R.drawable.com_choki)
-            pa -> comHandImage.setImageResource(R.drawable.com_pa)
-        }
+//        val comHand = (Math.random() * 3).toInt()
+//        when(comHand) {
+//            gu -> comHandImage.setImageResource(R.drawable.com_gu)
+//            choki -> comHandImage.setImageResource(R.drawable.com_choki)
+//            pa -> comHandImage.setImageResource(R.drawable.com_pa)
+//        }
 
         // 勝敗判定
         // ロジック （コンピュータの手 - プレイヤーの手 + 3）% 3 = 0:引き分け 1:勝ち 2:負け
@@ -55,5 +59,76 @@ class ResultActivity : AppCompatActivity() {
 
         // 戻るボタンをタップした時はじゃんけん画面に戻る
         backButton.setOnClickListener { finish() }
+
+        // 結果を保存
+        saveJankenData(myHand, comHand, gameResult)
+    }
+
+    /**
+     * saveJankenData
+     *
+     * １回ごとのじゃんけんごとの手と結果を保存する
+     */
+    private fun saveJankenData(myHand: Int, comHand: Int, gameResult: Int) {
+
+        // 保存されている結果データ（共有プリファレンス）を取得
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val gameCount = pref.getInt("GAME_COUNT", 0)
+        val winningStreakCount = pref.getInt("WINNING_STREAK_COUNT", 0)
+        val lastComHand = pref.getInt("LAST_COM_HAND", 0)
+        val lastGameresult = pref.getInt("GAME_RESULT", -1)
+
+        // 結果の書き込み
+        val editor = pref.edit()
+        editor.putInt("GAME_COUNT", gameCount + 1)
+                .putInt("WINNING_STREAK_COUNT",
+                        if(lastGameresult == 2 && gameResult == 2)
+                            winningStreakCount + 1
+                        else
+                            0)
+                .putInt("LAST_MY_HAND", myHand)
+                .putInt("LAST_COM_HAND", comHand)
+                .putInt("BEFORE_LAST_HAND", lastComHand)
+                .putInt("GAME_RESULT", gameResult)
+                .apply()
+    }
+
+    /**
+     * getComHand
+     *
+     * 過去のじゃんけんの結果からコンピュータの手を決定する
+     */
+    private fun getComHand(): Int {
+        var hand = (Math.random() * 3).toInt()
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val gameCount = pref.getInt("GAME_COUNT", 0)
+        val winningStreakCount = pref.getInt("WINNING_STREAK_COUNT", 0)
+        val lastMyHand = pref.getInt("LAST_MY_HAND", 0)
+        val lastComHand = pref.getInt("LAST_COM_HAND", 0)
+        val beforeLastComHand = pref.getInt("BEFORE_LAST_COM_HAND", 0)
+        val gameResult = pref.getInt("GAME_RESULT", -1)
+
+        if (gameCount == 1) {
+            if (gameResult == 2) {
+                // 前回の勝負が1回目で、コンピュータが勝った場合、
+                // コンピュータは次に出す手を変える
+                while (lastComHand == hand) {
+                    hand = (Math.random() * 3).toInt()
+                }
+            } else if (gameResult == 1) {
+                // 前回の勝負が1回目で、コンピュータが負けた場合
+                // 相手の出した手に勝つ手を出す
+                hand = (lastMyHand - 1 + 3) % 3
+            }
+        } else if (winningStreakCount > 0) {
+            if (beforeLastComHand == lastComHand) {
+                // 同じ手で連勝した場合は手を変える
+                while (lastComHand == hand) {
+                    hand = (Math.random() * 3).toInt()
+                }
+            }
+        }
+        return hand
     }
 }
